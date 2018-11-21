70c1ed40e1aae0572a2c55fc94747084a7830d9e
Vladimír Fojtík <fojtik.v@gmail.com>
2018-11-21 15:22:19 +0100

    Moved recommender api to it's own repo

--- recommender/resources/movie_recommendation.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-from recommenders.cb_recommender import CBRecommender
-
-class MovieRecommendation(Resource):
-    def get(self, id):
-        recommendations = CBRecommender.get_recommendations(id)
-        return recommendations, 200
--- recommender/resources/user_recommendation.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-from recommenders.cbf_recommender import CBFRecommender
-
-class UserRecommendation(Resource):
-    def get(self, id):
-        recommendations = CBFRecommender.get_recommendations(id)
-        return recommendations, 200
