d26654703e1caef75d9f8bc423535f51f53d1008
Vladimír Fojtík <fojtik.v@gmail.com>
2018-11-22 08:13:38 +0100

    Changed pm2 config to start also client

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,19 @@ module.exports = {
             name: 'API',
             instances: "2",
             exec_mode: "cluster",
-            script: "./server/src/index.ts",
+            script: "yarn",
+            args: "server:start",
+            env: {
+                COMMON_VARIABLE: 'true',
+                NODE_ENV: 'production'
+            }
+        },
+        {
+            name: 'Client',
+            instances: "2",
+            exec_mode: "cluster",
+            script: "yarn",
+            args: "client:start",
             env: {
                 COMMON_VARIABLE: 'true',
                 NODE_ENV: 'production'
