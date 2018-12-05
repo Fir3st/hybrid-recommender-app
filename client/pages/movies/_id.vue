9e3c77c654f9726e1eb76095c00ff80507b1508a
Vladimír Fojtík <fojtik.v@gmail.com>
2018-12-05 12:09:10 +0100

    Added recommended movies for user

--- client/pages/movies/_id.vue
+++ client/pages/movies/_id.vue
@@ -12,6 +12,12 @@
                 <movie-list :movies="similarMovies" />
             </b-col>
         </b-row>
+        <b-row v-if="isLogged">
+            <b-col>
+                <h2>Recommended for you</h2>
+                <movie-list :movies="recommendedMovies" />
+            </b-col>
+        </b-row>
     </div>
 </template>
 
@@ -31,27 +37,25 @@
         auth: false,
         data() {
             return {
-                similarMovies: []
+                similarMovies: [],
+                recommendedMovies: []
             };
         },
         computed: {
             ...mapGetters({
                 movie: 'movies/activeMovie'
             }),
+            isLogged() {
+                return !!this.$auth.user;
+            },
             movieTitle() {
                 return (this.movie && this.movie.title) ? this.movie.title : 'Detail';
             }
         },
         async mounted() {
-            const id = this.$route.params.id;
-            try {
-                const recommendations = await this.$axios.$get(`/movies/${id}/recommendations`);
-
-                if (recommendations && recommendations.length > 0) {
-                    this.similarMovies = recommendations;
-                }
-            } catch (error) {
-                console.log(error);
+            await this.getSimilarMovies();
+            if (this.isLogged) {
+                await this.getUserRecommendedMovies();
             }
         },
         async fetch ({ app, store, params }) {
@@ -63,6 +67,32 @@
                 }
             } catch (error) {
                 console.log(error);
+            }
+        },
+        methods: {
+            async getSimilarMovies() {
+                const id = this.$route.params.id;
+                try {
+                    const recommendations = await this.$axios.$get(`/movies/${id}/recommendations`);
+
+                    if (recommendations && recommendations.length > 0) {
+                        this.similarMovies = recommendations;
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+            async getUserRecommendedMovies() {
+                const id = this.$auth.user.id;
+                try {
+                    const recommendations = await this.$axios.$get(`/users/${id}/recommendations`);
+
+                    if (recommendations && recommendations.length > 0) {
+                        this.recommendedMovies = recommendations;
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
             }
         }
     };
