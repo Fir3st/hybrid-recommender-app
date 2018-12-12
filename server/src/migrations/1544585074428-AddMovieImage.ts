fdf10dcc6c06102ab0fc34aacff5accfb3067fc2
Vladimír Fojtík <fojtik.v@gmail.com>
2018-12-12 04:52:33 +0100

    Lint fix

--- server/src/migrations/1544585074428-AddMovieImage.ts
+++ server/src/migrations/1544585074428-AddMovieImage.ts
@@ -1,17 +1,17 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddMovieImage1544585074428 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query("ALTER TABLE `movies` ADD `image` varchar(255) NOT NULL");
-        await queryRunner.query("ALTER TABLE `users_ratings` CHANGE `rating` `rating` float NOT NULL");
-        await queryRunner.query("ALTER TABLE `users_ratings` CHANGE `createdAt` `createdAt` datetime NOT NULL");
+        await queryRunner.query('ALTER TABLE `movies` ADD `image` varchar(255) NOT NULL');
+        await queryRunner.query('ALTER TABLE `users_ratings` CHANGE `rating` `rating` float NOT NULL');
+        await queryRunner.query('ALTER TABLE `users_ratings` CHANGE `createdAt` `createdAt` datetime NOT NULL');
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query("ALTER TABLE `users_ratings` CHANGE `createdAt` `createdAt` datetime(0) NOT NULL");
-        await queryRunner.query("ALTER TABLE `users_ratings` CHANGE `rating` `rating` float(12) NOT NULL");
-        await queryRunner.query("ALTER TABLE `movies` DROP COLUMN `image`");
+        await queryRunner.query('ALTER TABLE `users_ratings` CHANGE `createdAt` `createdAt` datetime(0) NOT NULL');
+        await queryRunner.query('ALTER TABLE `users_ratings` CHANGE `rating` `rating` float(12) NOT NULL');
+        await queryRunner.query('ALTER TABLE `movies` DROP COLUMN `image`');
     }
 
 }
