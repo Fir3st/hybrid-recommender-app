84a510200441ba11c9c4116112a2c8e412fe5503
Vladimír Fojtík <fojtik.v@gmail.com>
2019-02-03 18:07:19 +0100

    Set up test for moviesController

---
+++ server/tests/controllers/moviesController.test.ts
@@ -0,0 +1,71 @@
+import { agent, Response, SuperTest, Test } from 'supertest';
+import { Connection, createConnection, getRepository, Repository } from 'typeorm';
+import app from '../../src/app';
+import { Movie } from '../../src/entities/Movie';
+import { Genre } from '../../src/entities/Genre';
+
+const request: SuperTest<Test> = agent(app);
+
+describe('GET /movies', () => {
+    let connection: Connection = null;
+    let repository: Repository<Movie> = null;
+
+    beforeAll(async () => {
+        try {
+            connection = await createConnection();
+            repository = getRepository(Movie);
+
+            const movie = new Movie();
+            movie.imdbId = 'tt0001';
+            movie.title = 'Test movie';
+            movie.year = new Date().getFullYear();
+            movie.rating = 'PG-13';
+            movie.releaseDate = new Date().toString();
+            movie.genres = [];
+            const genre = new Genre();
+            genre.name = 'Animation';
+            movie.genres.push(genre);
+            movie.director = 'John Smith';
+            movie.plot = 'Some interesting plot';
+            movie.poster = 'some poster link';
+            movie.type = 'movie';
+            movie.production = 'Some production name';
+
+            await repository.save(movie);
+        } catch (error) {
+            console.log(error.message);
+            throw new Error('Error while connecting to database');
+        }
+    }, 20000);
+
+    afterAll(async () => {
+        try {
+            await connection.dropDatabase();
+            await connection.close();
+        } catch (error) {
+            console.log(error.message);
+            throw new Error('Error while disconnecting from database');
+        }
+    }, 20000);
+
+    it('should respond with status code 200', async () => {
+        const response: Response = await request
+            .get('/movies');
+
+        expect(response.status).toBe(200);
+    });
+
+    it('should respond with status code 200 with genre', async () => {
+        const response: Response = await request
+            .get('/movies?genre=animation');
+
+        expect(response.status).toBe(200);
+    });
+
+    it('should respond with status code 200 with type', async () => {
+        const response: Response = await request
+            .get('/movies?type=movie');
+
+        expect(response.status).toBe(200);
+    });
+});
--- server/tests/controllers/usersController.test.ts
+++ server/tests/controllers/usersController.test.ts
@@ -6,6 +6,7 @@ const request: SuperTest<Test> = agent(app);
 describe('GET /users', () => {
     it('should respond with status 401 if request is not authenticated', async () => {
         const response: Response = await request.get('/users');
+
         expect(response.status).toBe(401);
     });
 });
