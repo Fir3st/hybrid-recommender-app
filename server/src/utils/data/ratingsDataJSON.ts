77675ec2ec1004e073331e0a141a455a9872c771
Vladimír Fojtík <fojtik.v@gmail.com>
2019-02-19 06:52:51 +0100

    Added preloaders for json data files (for testing)

---
+++ server/src/utils/data/ratingsDataJSON.ts
@@ -0,0 +1,23 @@
+import * as csv from 'csvtojson';
+import * as fs from 'fs';
+
+csv()
+    .fromFile('server/src/utils/data/ratings.csv')
+    .then((data) => {
+        const ratings = [];
+
+        for (const rating of data) {
+            try {
+                ratings.push({
+                    userId: parseInt(rating.userId, 10),
+                    movieId: parseInt(rating.movieId, 10),
+                    rating: parseFloat(rating.rating),
+                    timestamp: rating.timestamp
+                });
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+
+        fs.writeFile('server/src/utils/data/ratings_data.json', JSON.stringify({ data: ratings }), 'utf8', () => console.log('Done.'));
+    });
