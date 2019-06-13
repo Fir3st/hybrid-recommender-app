9d4685be8a340c6c3302413896ccffe17e8b8560
Vladimír Fojtík <fojtik.v@gmail.com>
2019-06-13 06:44:42 +0200

    Added downloading recommendations as CSV for specific genre

--- client/components/admin/users/UserAnalysis.vue
+++ client/components/admin/users/UserAnalysis.vue
@@ -57,13 +57,22 @@
                         v-model="genreId"
                         :options="options"
                     ></b-form-select>
-                    <b-button
-                        class="btn-rec"
-                        variant="outline-primary"
-                        @click="getRecommendationsForGenre"
-                    >
-                        Get recommendations
-                    </b-button>
+                    <div v-if="genreId !== null">
+                        <b-button
+                            class="btn-rec"
+                            variant="info"
+                            @click="getRecommendationsForGenre"
+                        >
+                            Get recommendations
+                        </b-button>
+                        <b-button
+                            class="btn-rec"
+                            variant="info"
+                            @click="downloadGenreRecommendationsCSV"
+                        >
+                            Download recommendations for this genre as CSV
+                        </b-button>
+                    </div>
                 </b-col>
             </b-row>
             <b-row v-if="genreRecommendations">
@@ -158,22 +167,50 @@
                 }
             },
             async downloadRecommendationsCSV() {
-                const response = await this.$axios.$get(`/users/${this.user.id}/recommendations?take=100`);
-                const recs = response.map((item) => {
-                    return {
-                        id: item.id,
-                        title: item.title,
-                        predicted_rating: item.rating,
-                        average_rating: item.avgRating,
-                        count: parseInt(item.ratingsCount, 10)
-                        // TODO: add number of penalizations (also on API)
-                        // TODO: add similarity to already rated movies
-                    };
-                });
+                try {
+                    const response = await this.$axios.$get(`/users/${this.user.id}/recommendations?take=100`);
+                    if (response && response.length > 0) {
+                        const recs = response.map((item) => {
+                            return {
+                                id: item.id,
+                                title: item.title,
+                                predicted_rating: item.rating,
+                                average_rating: item.avgRating,
+                                count: parseInt(item.ratingsCount, 10)
+                                // TODO: add number of penalizations (also on API)
+                                // TODO: add similarity to already rated movies
+                            };
+                        });
 
-                this.downloadCSV(recs, `recommendations_user_${this.user.id}.csv`);
+                        this.downloadCSV(recs, `recommendations_user_${this.user.id}.csv`);
+                    }
+                } catch (error) {
+                    console.log(error.message);
+                }
+            },
+            async downloadGenreRecommendationsCSV() {
+                try {
+                    const url = `/users/${this.user.id}/recommendations?genres=${this.genreId}&take=100`;
+                    const response = await this.$axios.$get(url);
+                    if (response && response.length > 0) {
+                        const recs = response.map((item) => {
+                            return {
+                                id: item.id,
+                                title: item.title,
+                                predicted_rating: item.rating,
+                                average_rating: item.avgRating,
+                                count: parseInt(item.ratingsCount, 10)
+                                // TODO: add number of penalizations (also on API)
+                                // TODO: add similarity to already rated movies
+                            };
+                        });
+
+                        this.downloadCSV(recs, `recommendations_user_${this.user.id}_genre_${this.genreId}.csv`);
+                    }
+                } catch (error) {
+                    console.log(error.message);
+                }
             }
-            // TODO: add downloading recommendations for genre as CSV
         }
     };
 </script>
