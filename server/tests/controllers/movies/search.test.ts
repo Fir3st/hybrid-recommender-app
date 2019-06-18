0dee673f780a0411831d7bf95f25284f109320b1
Vladimír Fojtík <fojtik.v@gmail.com>
2019-06-18 12:38:52 +0200

    Added tests for secured search by query

--- server/tests/controllers/movies/search.test.ts
+++ server/tests/controllers/movies/search.test.ts
@@ -1,3 +1,6 @@
+import * as moxios from 'moxios';
+import * as config from 'config';
+import * as bcrypt from 'bcrypt';
 import { agent, Response, SuperTest, Test } from 'supertest';
 import { Connection, createConnection, getRepository, Repository } from 'typeorm';
 import app from '../../../src/app';
@@ -18,13 +21,13 @@ const user = new User();
 user.name = 'Test';
 user.surname = 'Test';
 user.email = 'test@test.co';
-user.password = 'secret';
+user.password = bcrypt.hashSync('secret', 10);
 
 const user2 = new User();
 user2.name = 'Test2';
 user2.surname = 'Test2';
-user2.email = 'test@test.co';
-user2.password = 'secret';
+user2.email = 'test2@test.co';
+user2.password = bcrypt.hashSync('secret', 10);
 
 const movie = new Movie();
 movie.imdbId = 'tt0001';
@@ -201,5 +204,167 @@ describe('Search - search by genre', () => {
         expect(movie).toHaveProperty('avgRating');
         expect(movie).toHaveProperty('ratingsCount');
         expect(movie).toHaveProperty('penalized');
+    });
+});
+
+describe('Secured search - search by query', () => {
+    let connection: Connection = null;
+    let repository: Repository<Movie> = null;
+    let token: string = '';
+
+    beforeAll(async () => {
+        try {
+            connection = await createConnection();
+            repository = getRepository(Movie);
+            const usersRepository = getRepository(User);
+            await usersRepository.save(user);
+            await usersRepository.save(user2);
+            await repository.save(movie);
+            await repository.save(movie2);
+            const response: Response = await request
+                .post('/auth/login')
+                .type('form')
+                .send({ email: user.email, password: 'secret' });
+            if (response.status === 200) {
+                token = response.body.token;
+            }
+        } catch (error) {
+            throw new Error(error.message);
+        }
+        moxios.install();
+    }, 20000);
+
+    afterAll(async () => {
+        try {
+            await connection.dropDatabase();
+            await connection.close();
+        } catch (error) {
+            throw new Error(error.message);
+        }
+        moxios.uninstall();
+    }, 20000);
+
+    it('should respond with status code 200', async () => {
+        const recommenderUrl = config.get('recommenderUrl');
+        moxios.stubRequest(`${recommenderUrl}/search/1`, {
+            status: 200,
+            response: {
+                ratings: [
+                    { id: 1, rating: 0.5, similarity: 0.05 },
+                    { id: 2, rating: 1.5, similarity: 0.15 }
+                ]
+            }
+        });
+        const response: Response = await request
+            .get('/movies/secured-search?query=test')
+            .set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(200);
+    });
+
+    it('should respond with status code 401 without Authorization header', async () => {
+        const response: Response = await request
+            .get('/movies/secured-search?query=test');
+
+        expect(response.status).toBe(401);
+    });
+
+    it('number of movies in response should match number of movies in database', async () => {
+        const recommenderUrl = config.get('recommenderUrl');
+        moxios.stubRequest(`${recommenderUrl}/search/1`, {
+            status: 200,
+            response: {
+                ratings: [
+                    { id: 1, rating: 0.5, similarity: 0.05 },
+                    { id: 2, rating: 1.5, similarity: 0.15 }
+                ]
+            }
+        });
+        const searchQuery = 'test';
+        const response: Response = await request
+            .get(`/movies/secured-search?query=${searchQuery}`)
+            .set('Authorization', `Bearer ${token}`);
+        const query = repository
+            .createQueryBuilder('movies')
+            .where('LOWER(movies.title) LIKE :query', { query: `%${searchQuery}%` });
+        const count = await query.getCount();
+        const { movies } = response.body;
+
+        expect(movies).toBeDefined();
+        expect(Array.isArray(movies)).toBeTruthy();
+        expect(movies.length).toEqual(count);
+    });
+
+    it('should respond with status code 400 for title that doesn\'t exist', async () => {
+        const response: Response = await request
+            .get('/movies/secured-search?query=x-men')
+            .set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(400);
+    });
+
+    it('response should have stats', async () => {
+        const recommenderUrl = config.get('recommenderUrl');
+        moxios.stubRequest(`${recommenderUrl}/search/1`, {
+            status: 200,
+            response: {
+                ratings: [
+                    { id: 1, rating: 0.5, similarity: 0.05 },
+                    { id: 2, rating: 1.5, similarity: 0.15 }
+                ]
+            }
+        });
+        const response: Response = await request
+            .get('/movies/secured-search?query=test')
+            .set('Authorization', `Bearer ${token}`);
+        const { movies } = response.body;
+        const movie = movies[0];
+
+        expect(movie).toHaveProperty('avgRating');
+        expect(movie).toHaveProperty('ratingsCount');
+        expect(movie).toHaveProperty('penalized');
+    });
+
+    it('stats from recommender should correspond with stats in response', async () => {
+        const recommenderUrl = config.get('recommenderUrl');
+        const ratings = [
+            { id: 1, rating: 0.5, similarity: 0.05 },
+            { id: 2, rating: 1.5, similarity: 0.15 }
+        ];
+        moxios.stubRequest(`${recommenderUrl}/search/1`, {
+            status: 200,
+            response: {
+                ratings
+            }
+        });
+        const response: Response = await request
+            .get('/movies/secured-search?query=test')
+            .set('Authorization', `Bearer ${token}`);
+        const { movies } = response.body;
+        const movie = movies[0];
+        const ratingForMovie = ratings.find(item => item.id === movie.id);
+
+        expect(movie.rating).toEqual(ratingForMovie.rating);
+        expect(movie.ratedSimilarity).toEqual(ratingForMovie.similarity);
+    });
+
+    it('items in response should be ordered by rating', async () => {
+        const recommenderUrl = config.get('recommenderUrl');
+        const ratings = [
+            { id: 1, rating: 0.5, similarity: 0.05 },
+            { id: 2, rating: 1.5, similarity: 0.15 }
+        ];
+        moxios.stubRequest(`${recommenderUrl}/search/1`, {
+            status: 200,
+            response: {
+                ratings
+            }
+        });
+        const response: Response = await request
+            .get('/movies/secured-search?query=test')
+            .set('Authorization', `Bearer ${token}`);
+        const { movies } = response.body;
+
+        expect(movies[0].rating > movies[1].rating).toBeTruthy();
     });
 });
