322f5167397ba6ffa9a098a537929c5144636cbd
Vladimír Fojtík <fojtik.v@gmail.com>
2019-06-18 07:45:36 +0200

    Added testing for MoviesUtil getStats

--- server/tests/utils/moviesUtil.test.ts
+++ server/tests/utils/moviesUtil.test.ts
@@ -1,4 +1,9 @@
+import * as moxios from 'moxios';
+import { Connection, createConnection, getRepository, Repository } from 'typeorm';
 import MoviesUtil from '../../src/utils/movies/MoviesUtil';
+import { Movie } from '../../src/entities/Movie';
+import { User } from '../../src/entities/User';
+import { UserRating } from '../../src/entities/UserRating';
 
 const movies = [
     { id: 1, title: 'test 1' },
@@ -144,5 +149,100 @@ describe('isPenalizedByUser', () => {
         const status = MoviesUtil.isPenalizedByUser(movie, user);
 
         expect(status.isPenalized).toBeFalsy();
+    });
+});
+
+describe('getStats', () => {
+    let connection: Connection = null;
+    let repository: Repository<Movie> = null;
+
+    const user = new User();
+    user.name = 'Test';
+    user.surname = 'Test';
+    user.email = 'test@test.co';
+    user.password = 'secret';
+    const user2 = new User();
+    user2.name = 'Test2';
+    user2.surname = 'Test2';
+    user2.email = 'test@test.co';
+    user2.password = 'secret';
+
+    const movie = new Movie();
+    movie.imdbId = 'tt0001';
+    movie.title = 'Test movie';
+    movie.year = new Date().getFullYear();
+    movie.rating = 'PG-13';
+    movie.releaseDate = new Date().toString();
+    movie.director = 'John Smith';
+    movie.plot = 'Some interesting plot';
+    movie.poster = 'some poster link';
+    movie.type = 'movie';
+    movie.production = 'Some production name';
+    movie.usersRatings = [];
+
+    const userRating = new UserRating();
+    userRating.user = user;
+    userRating.rating = 1;
+    userRating.createdAt = new Date().toString();
+    const userRating2 = new UserRating();
+    userRating2.user = user2;
+    userRating2.rating = 0;
+    userRating2.createdAt = new Date().toString();
+
+    movie.usersRatings.push(userRating);
+    movie.usersRatings.push(userRating2);
+
+    beforeAll(async () => {
+        try {
+            connection = await createConnection();
+            const usersRepository = getRepository(User);
+            await usersRepository.save([user, user2]);
+            repository = getRepository(Movie);
+            await repository.save(movie);
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }, 100000);
+    afterAll(async () => {
+        try {
+            await connection.dropDatabase();
+            await connection.close();
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }, 20000);
+
+    it('result should contains stats for movie in database', async () => {
+        const movies = [movie];
+        const moviesWithStats = await MoviesUtil.getStats(movies);
+
+        expect(moviesWithStats[0]).toHaveProperty('avgRating');
+        expect(moviesWithStats[0]).toHaveProperty('ratingsCount');
+        expect(moviesWithStats[0]).toHaveProperty('penalized');
+    });
+
+    it('movie should be penalized by one rating', async () => {
+        const movies = [movie];
+        const moviesWithStats = await MoviesUtil.getStats(movies);
+
+        expect(moviesWithStats[0]).toHaveProperty('penalized');
+        expect(moviesWithStats[0].penalized).toEqual(1);
+    });
+
+    it('number of ratings in stats should match actual number of ratings', async () => {
+        const movies = [movie];
+        const moviesWithStats = await MoviesUtil.getStats(movies);
+
+        expect(moviesWithStats[0]).toHaveProperty('ratingsCount');
+        expect(moviesWithStats[0].ratingsCount).toEqual(movie.usersRatings.length);
+    });
+
+    it('average rating in stats should match actual average rating', async () => {
+        const movies = [movie];
+        const moviesWithStats = await MoviesUtil.getStats(movies);
+        const ratingsSum = movie.usersRatings.reduce((value, item) => value + item.rating, 0);
+
+        expect(moviesWithStats[0]).toHaveProperty('avgRating');
+        expect(moviesWithStats[0].avgRating).toEqual(ratingsSum / movie.usersRatings.length);
     });
 });
