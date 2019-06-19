91d121d4103e0bdab7d06ccbcad4ed8a5262da91
Vladimír Fojtík <fojtik.v@gmail.com>
2019-06-19 07:38:53 +0200

    Refactored tests for genres

--- server/tests/controllers/genres/basics.test.ts
+++ server/tests/controllers/genres/basics.test.ts
@@ -3,33 +3,22 @@ import { Connection, createConnection, getRepository, Repository } from 'typeorm
 import app from '../../../src/app';
 import { Movie } from '../../../src/entities/Movie';
 import { Genre } from '../../../src/entities/Genre';
+import { genre, movie } from '../../helpers';
 
 const request: SuperTest<Test> = agent(app);
 
-const movie = new Movie();
-movie.imdbId = 'tt0001';
-movie.title = 'Test movie';
-movie.year = new Date().getFullYear();
-movie.rating = 'PG-13';
-movie.releaseDate = new Date().toString();
-movie.genres = [];
-const genre = new Genre();
-genre.name = 'Animation';
 movie.genres.push(genre);
-movie.director = 'John Smith';
-movie.plot = 'Some interesting plot';
-movie.poster = 'some poster link';
-movie.type = 'movie';
-movie.production = 'Some production name';
 
 describe('Basics for /genres', () => {
     let connection: Connection = null;
     let repository: Repository<Movie> = null;
+    let genresRepository: Repository<Genre> = null;
 
     beforeAll(async () => {
         try {
             connection = await createConnection();
             repository = getRepository(Movie);
+            genresRepository = getRepository(Genre);
             await repository.save(movie);
         } catch (error) {
             throw new Error(error.message);
@@ -60,8 +49,6 @@ describe('Basics for /genres', () => {
     });
 
     it('number of genres should correspond to number of genres of movie', async () => {
-        const genresRepository = getRepository(Genre);
-
         const response: Response = await request
             .get('/genres?type=movie');
         const query = genresRepository
@@ -77,8 +64,6 @@ describe('Basics for /genres', () => {
     });
 
     it('genres in respond should correspond to genres in database', async () => {
-        const genresRepository = getRepository(Genre);
-
         const response: Response = await request
             .get('/genres?type=movie');
         const query = genresRepository
