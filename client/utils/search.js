474ba1f4b02f7ad168e5fe42f4ee8f20e891061b
Vladimír Fojtík <fojtik.v@gmail.com>
2019-07-18 15:12:10 +0200

    Fix for search

--- client/utils/search.js
+++ client/utils/search.js
@@ -1,11 +1,13 @@
 export const containedGenres = (genres, searchQuery) => {
+    const query = searchQuery.toLowerCase();
     genres = genres.map(genre => genre.name.toLowerCase()).join('|');
 
-    return searchQuery.match(new RegExp(genres, 'g'));
+    return query.match(new RegExp(genres, 'g'));
 };
 
 export const containedTypes = (types, searchQuery) => {
+    const query = searchQuery.toLowerCase();
     types = types.map(type => type.toLowerCase()).join('|');
 
-    return searchQuery.match(new RegExp(types, 'g'));
+    return query.match(new RegExp(types, 'g'));
 };
