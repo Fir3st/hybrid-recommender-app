c1f9aa193dbef60ed67df68959b4e5a1390f0ac6
Fir3st <fojtik.v@gmail.com>
2019-10-24 16:23:20 +0200

    Added fav genres and not fav genres to user's detail

--- client/components/admin/users/UserDetail.vue
+++ client/components/admin/users/UserDetail.vue
@@ -33,6 +33,22 @@
                     {{ user.admin ? 'Admin' : 'User' }}
                 </b-col>
             </b-row>
+            <b-row v-if="favGenres.length">
+                <b-col sm="2">
+                    Favourite genres:
+                </b-col>
+                <b-col sm="10">
+                    {{ favGenres.join(', ') }}
+                </b-col>
+            </b-row>
+            <b-row v-if="notFavGenres.length">
+                <b-col sm="2">
+                    Favourite genres:
+                </b-col>
+                <b-col sm="10">
+                    {{ notFavGenres.join(', ') }}
+                </b-col>
+            </b-row>
         </b-col>
     </b-row>
 </template>
@@ -43,6 +59,25 @@
             user: {
                 type: Object,
                 required: true
+            }
+        },
+        computed: {
+            favGenres() {
+                return this.getGenresByType(1);
+            },
+            notFavGenres() {
+                return this.getGenresByType(-1);
+            }
+        },
+        methods: {
+            getGenresByType(type = 1) {
+                const genres = [];
+                if (this.user.favouriteGenres && this.user.favouriteGenres.length > 0) {
+                    const favGenres = this.user.favouriteGenres.filter(item => item.type === type).map(item => item.genre.name);
+                    genres.push(...favGenres);
+                }
+
+                return genres;
             }
         }
     };
