57cc94311045e34e01a9b782dccdbebf00e79051
Fir3st <fojtik.v@gmail.com>
2019-11-04 19:18:44 +0100

    Added more stats to results page

--- client/pages/admin/results/_id.vue
+++ client/pages/admin/results/_id.vue
@@ -93,22 +93,24 @@
                         <h3>Content-based results</h3>
                         <p><strong>Relevant:</strong> {{ countRelevance('cbResults', 'relevant') }}</p>
                         <p><strong>Not relevant:</strong> {{ countRelevance('cbResults', 'notRelevant') }}</p>
-                        <p><strong>Final relevance: </strong> {{ getFinalScore(countRelevance('cbResults', 'relevant')) }}%</p>
+                        <p><strong>Precision:</strong> {{ getFinalScore(countRelevance('cbResults', 'relevant')) }}%</p>
 
                         <h3>Collaborative results</h3>
                         <p><strong>Relevant:</strong> {{ countRelevance('cbfResults', 'relevant') }}</p>
                         <p><strong>Not relevant:</strong> {{ countRelevance('cbfResults', 'notRelevant') }}</p>
-                        <p><strong>Final relevance: </strong> {{ getFinalScore(countRelevance('cbfResults', 'relevant')) }}%</p>
+                        <p><strong>Precision:</strong> {{ getFinalScore(countRelevance('cbfResults', 'relevant')) }}%</p>
 
                         <h3>Hybrid results</h3>
                         <p><strong>Relevant:</strong> {{ countRelevance('hybridResults', 'relevant') }}</p>
                         <p><strong>Not relevant:</strong> {{ countRelevance('hybridResults', 'notRelevant') }}</p>
-                        <p><strong>Final relevance: </strong> {{ getFinalScore(countRelevance('hybridResults', 'relevant')) }}%</p>
+                        <p><strong>Precision:</strong>  {{ getFinalScore(countRelevance('hybridResults', 'relevant')) }}%</p>
 
-                        <h3>Expert results</h3>
+                        <h3>Hybrid system (expert) results</h3>
                         <p><strong>Relevant:</strong> {{ countRelevance('expertResults', 'relevant') }}</p>
                         <p><strong>Not relevant:</strong> {{ countRelevance('expertResults', 'notRelevant') }}</p>
-                        <p><strong>Final relevance: </strong> {{ getFinalScore(countRelevance('expertResults', 'relevant')) }}%</p>
+                        <p><strong>Precision:</strong> {{ getFinalScore(countRelevance('expertResults', 'relevant')) }}%</p>
+                        <p><strong>Recall:</strong> {{ getRecallScore('expertResults', 15) }}%</p>
+                        <p><strong>F1-Measure:</strong> {{ getFMeasureScore('expertResults', 15) }}%</p>
                     </b-col>
                 </b-row>
                 <b-row>
@@ -139,7 +141,7 @@
                             :items="hybridData"
                         />
 
-                        <h3>Expert results</h3>
+                        <h3>Hybrid system (expert) results</h3>
                         <b-table
                             striped
                             hover
@@ -253,7 +255,20 @@
             },
             getFinalScore(relevant) {
                 const total = this.result.settings.general.take;
-                return (relevant / total) * 100;
+                return Math.round((relevant / total) * 100);
+            },
+            getRecallScore(system, n) {
+                const topN = this.result.results[system].slice(0, n);
+                const relevantCount = topN.reduce((count, item) => {
+                    return item.relevance ? count + 1 : count;
+                }, 0);
+
+                return Math.round((relevantCount / n) * 100);
+            },
+            getFMeasureScore(system, n) {
+                const precision = this.getFinalScore(this.countRelevance(system, 'relevant'));
+                const recall = this.getRecallScore(system, n);
+                return Math.round((2 * precision * recall) / (precision + recall));
             }
         }
     };
