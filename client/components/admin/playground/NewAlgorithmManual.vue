491e1a89e94b8d1ee7ea889fa4bc23308fc19507
Vladimír Fojtík <fojtik.v@gmail.com>
2020-11-14 11:36:12 +0100

    metrics

--- client/components/admin/playground/NewAlgorithmManual.vue
+++ client/components/admin/playground/NewAlgorithmManual.vue
@@ -18,6 +18,13 @@
             b-row(v-if="!loading && movies.length", class="mt-2 mb-2")
                 b-col
                     h3 Recommended movies
+                    p Relevant: {{ relevantCount }}
+                    p Not relevant: {{ notRelevantCount }}
+                    p Precision: {{ precision }}
+                    p Recall (TOP 10): {{ recall10 }}
+                    p F1-Measure (TOP 10): {{ f1Measure10 }}
+                    p Recall (TOP 15): {{ recall15 }}
+                    p F1-Measure (TOP 15): {{ f1Measure15 }}
                     b-table(striped, hover, :items="movies", :fields="moviesFields")
                         template(v-slot:cell(#)="data") {{ data.index + 1 }}
 
@@ -45,12 +52,20 @@
                     { key: 'id', label: 'ID' },
                     { key: 'title', label: 'Title' },
                     { key: 'predictedRating', label: 'Rating' },
-                    { key: 'similarity', label: 'Similarity' }
+                    { key: 'similarity', label: 'Similarity' },
+                    { key: 'relevant', label: 'Relevant' }
                 ],
                 genres: [],
                 movies: [],
                 toTake: 25,
-                compareTo: ''
+                compareTo: '',
+                relevantCount: null,
+                notRelevantCount: null,
+                precision: null,
+                recall10: null,
+                recall15: null,
+                f1Measure10: null,
+                f1Measure15: null
             };
         },
         computed: {
@@ -172,24 +187,38 @@
                 }
             },
             async getRecommendations() {
-                this.loading = true;
                 this.favouriteItems = [];
                 this.genres = [];
 
                 try {
+                    await this.analyzeUser();
+                    this.loading = true;
                     const url = `/playground/users/${this.userId}?take=${this.toTake}&compareTo=${this.compareTo}`;
 
                     const response = await this.$axios.$get(url);
 
                     if (response && response.length) {
                         this.movies = response;
+                        const relevance = this.getRelevance(this.movies, this.mostRatedGenresAll, this.leastRatedGenresAll);
+                        this.relevantCount = this.getRelevantCount(relevance);
+                        this.notRelevantCount = this.toTake - this.relevantCount;
+                        this.precision = this.getFinalScore(this.toTake, this.relevantCount);
+                        this.recall10 = this.getRecallScore(relevance, 10);
+                        this.recall15 = this.getRecallScore(relevance, 15);
+                        this.f1Measure10 = this.getFMeasureScore(relevance, this.toTake, 10);
+                        this.f1Measure15 = this.getFMeasureScore(relevance, this.toTake, 15);
+                        this.movies = this.movies.map((movie) => {
+                            const relevanceForItem = relevance.find(item => item.id === movie.id);
+
+                            return { ...movie, relevant: relevanceForItem ? relevanceForItem.relevant : null };
+                        });
                     }
                 } catch (error) {
                     console.log(error.message);
                 } finally {
                     this.loading = false;
                 }
-            },
+            }
         }
     };
 </script>
--- client/plugins/globalMethods.js
+++ client/plugins/globalMethods.js
@@ -22,6 +22,50 @@ Vue.mixin({
                 encoding: 'utf8'
             });
             Download(parsedData, name, 'application/csv');
+        },
+        getRelevance(movies, mostRatedGenresAll, leastRatedGenresAll) {
+            const relevantGenres = mostRatedGenresAll.map(genre => genre.id);
+            const notRelevantGenres = leastRatedGenresAll.map(genre => genre.id);
+
+            return movies.map((movie) => {
+                const genres = movie.genres.map(genre => genre.id);
+
+                const movieRelevantGenres = genres.filter(genre => relevantGenres.includes(genre));
+                const movieNotRelevantGenres = genres.filter(genre => notRelevantGenres.includes(genre));
+
+                let relevant;
+                if (movieRelevantGenres.length > 0 && movieNotRelevantGenres.length === 0) {
+                    relevant = true;
+                } else if (movieRelevantGenres.length === 0 && movieNotRelevantGenres.length > 0) {
+                    relevant = false;
+                } else if (movieRelevantGenres.length > 0 && movieNotRelevantGenres.length > 0) {
+                    relevant = movieRelevantGenres.length > movieNotRelevantGenres.length;
+                }
+
+                return { id: movie.id, relevant };
+            });
+        },
+        getRelevantCount(items) {
+            return items.reduce((count, item) => {
+                return item.relevant ? count + 1 : count;
+            }, 0);
+        },
+        getFinalScore(toTake, relevant) {
+            return Math.round((relevant / toTake) * 100);
+        },
+        getRecallScore(results, n) {
+            const topN = results.slice(0, n);
+            const relevantCount = topN.reduce((count, item) => {
+                return item.relevant ? count + 1 : count;
+            }, 0);
+
+            return Math.round((relevantCount / n) * 100);
+        },
+        getFMeasureScore(results, toTake, n) {
+            const relevantItems = results.filter(item => item.relevant).length;
+            const precision = this.getFinalScore(toTake, relevantItems);
+            const recall = this.getRecallScore(results, n);
+            return Math.round((2 * precision * recall) / (precision + recall));
         }
     }
 });
