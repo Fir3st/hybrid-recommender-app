cdae1923ecef8dc8f30f21fd25e9ffefe3293baf
Vladimír Fojtík <fojtik.v@gmail.com>
2021-01-09 08:35:45 +0100

    number of users in group

--- client/components/admin/users/UserGroupTab.vue
+++ client/components/admin/users/UserGroupTab.vue
@@ -1,7 +1,12 @@
 <template lang="pug">
-    b-row
-        b-col
-            b-table(v-if="group.users && group.users.length", :items="group.users")
+    div
+        b-row(v-if="group.users && group.users.length", class="mb-3")
+            b-col
+                strong Number of users in group:
+                span  {{ group.users.length }}
+        b-row
+            b-col
+                b-table(v-if="group.users && group.users.length", :items="group.users")
 </template>
 
 <script>
